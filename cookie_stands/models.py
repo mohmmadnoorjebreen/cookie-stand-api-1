66e265d7ae82cf5d96ff754d94932d820bef854d
MOH <mohammadnoormjk1998@gmail.com>
2021-09-19 15:00:40 +0300

    update

--- cookie_stands/models.py
+++ cookie_stands/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class CookieStand(models.Model):
 
-    hourly_sales = models.JSONField(default=list, blank=True)
+    hourly_sales = models.TextField()
 
     def __str__(self):
         return self.name
